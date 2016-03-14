28ee8550619161b79c0aef77477e84312dfbdfb2
shivateja-madipalli <madipallishivateja@gmail.com>
2016-03-14 02:22:33 -0700

    New get request for retrieving all 'GIT' repos

--- mainserver.js
+++ mainserver.js
@@ -3,6 +3,7 @@ var Client = require("node-rest-client").Client;
 var mongoose = require("mongoose");
 var MongoClient = require('mongodb').MongoClient;
 
+
 var client = new Client();
 
 var app = express();
@@ -10,6 +11,9 @@ var app = express();
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+//https://api.github.com/users/shivateja-madipalli/repos?type=all&sort=created
+var githubUrl = "https://api.github.com/users/shivateja-madipalli/repos";
 
 
 //globally allowing CORS
@@ -48,8 +52,47 @@ app.get('/shivatejam/home/Description', function(req,res){
 });
 
 
+app.get('/shivatejam/githubrepos', function(req,res){
+  res.header('Access-Control-Allow-Origin','*');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+    //get from githubrepos
+
+    githubUrl + "?type=all&sort=created";
+     
+    var headerForRequest = {
+    headers: {'user-agent': 'node.js'}
+    }
+    client.get(githubUrl,headerForRequest,function(Data,Response){
+            console.log("github data: " + Data);
+            var finalResult = editGitJson(Data);
+            console.log("final result: " + finalResult);
+            res.json(finalResult);
+    });
+});
+
 //app.listen(8235);
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
 console.log('Server Started on 8235');
+
+
+function editGitJson(Data){
+    var requiredjsondata = [];
+    // requiredjsondata = "{";
+    for(var i=0;i<Data.length;i++)
+    {
+        var gitProj = "{";
+            gitProj += "name : " + Data[i].name + ",";
+            gitProj += "html_url : " + Data[i].html_url + ",";
+            gitProj += "description : " + Data[i].description + ",";
+            gitProj += "created_at : " + Data[i].created_at + ",";
+            gitProj += "language : " + Data[i].language;
+        gitProj += "}";
+        requiredjsondata.push(gitProj);
+    }
+    // requiredjsondata = "}";
+    return JSON.stringify(requiredjsondata);
+}
